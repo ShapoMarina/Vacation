553e4a697f76d7bab230ade2e55e5a983b9b499f
Marina Shapovalova <snyzinka@gmail.com>
2023-03-22 00:14:41 +0300

    Second commit

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,23 +2,19 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
-    @Test
-            public void shouldCalcVacation() {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void shouldCalcVacation(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000,3_000,20_000);
-    Assertions.assertEquals(expected, actual);
-    }
 
-    @Test
-    public void shouldCalcSecondVacation() {
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000,60_000,150_000);
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
